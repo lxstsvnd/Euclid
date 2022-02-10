8ec010722e06bdae85e2bfa6c10de47a4cc9ef29
deepblueore <79878872+deepblueore@users.noreply.github.com>
2022-02-10 03:26:43 +0300

    Add files via upload

---
+++ lexer.hpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <string>
+#include <cctype>
+
+class Token
+{
+	private:
+		std::string _token_name; //имя лексемы
+	public:
+		Token(std::string token_name);
+		~Token();
+		std::string get_name();
+};
+
+class Tokenizer
+{
+	private:
+		std::vector<Token> _tokens;
+		std::vector<char> _variables; //имена переменных (нужно для анализатора)
+		std::vector<int> _constants; //значения констант (нужно для анализатора)
+	public:
+		Tokenizer();
+		~Tokenizer();
+		void make_tokens(std::string input); //лексический анализ строки
+		std::vector<char> get_variables();
+		std::vector<int> get_constants();
+		std::vector<Token> get_tokens();
+		void add_variable(char symbol);
+		void add_constant(int constant);
+		void add_token(std::string token_name);
+		bool check_special(std::string special); //проверка строки на соответствие спецсимволу
+		bool isother(char symbol); //проверка символа на скобку, арифметический оператор или оператор порядка
+};
